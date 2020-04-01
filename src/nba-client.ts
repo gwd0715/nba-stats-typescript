432cf33e253d55f37179fdaf2a85b75570c0b9e6
Chris <gwd0715@gmail.com>
2020-04-01 10:17:07 +0800

    change backend URL

--- src/nba-client.ts
+++ src/nba-client.ts
@@ -1,36 +1,36 @@
 import nba from 'nba';
 
-const SERVER_URL = 'http://35.235.84.235:5000';
+const SERVER_URL = 'https://34.67.117.84';
 
 export interface IPlayerInfo {
-	teamAbbreviation?: string;
-	teamCity?: string;
-	teamName?: string;
-	playerName: string;
-	height?: number;
-	weight?: number;
-	playerId?: number;
-	pts?: number;
-	reb?: number;
-	ast?: number;
-	pie?: number;
+    teamAbbreviation?: string;
+    teamCity?: string;
+    teamName?: string;
+    playerName: string;
+    height?: number;
+    weight?: number;
+    playerId?: number;
+    pts?: number;
+    reb?: number;
+    ast?: number;
+    pie?: number;
 }
 
 type TPlayerID = {
-	PlayerID: number;
+    PlayerID: number;
 };
 
 export default {
-	...nba,
-	stats: {
-		...nba.stats,
-		playerInfo: async ({ PlayerID }: TPlayerID) => {
-			const res = await fetch(`${SERVER_URL}/players/${PlayerID}`);
-			return res.json();
-		},
-		shots: async ({ PlayerID }: TPlayerID) => {
-			const res = await fetch(`${SERVER_URL}/players/${PlayerID}/shots`);
-			return res.json();
-		}
-	}
+    ...nba,
+    stats: {
+        ...nba.stats,
+        playerInfo: async ({ PlayerID }: TPlayerID) => {
+            const res = await fetch(`${SERVER_URL}/players/${PlayerID}`);
+            return res.json();
+        },
+        shots: async ({ PlayerID }: TPlayerID) => {
+            const res = await fetch(`${SERVER_URL}/players/${PlayerID}/shots`);
+            return res.json();
+        }
+    }
 };
